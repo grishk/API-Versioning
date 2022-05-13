4eff4989283816926882bbe72eecec2ed62d0634
OLGR <flash.inc@mail.com>
2022-05-13 12:34:55 +0200

    People API

---
+++ ODataRuntime.Impl/ApiControllers/PeopleApi.cs
@@ -0,0 +1,16 @@
+﻿using ODataRuntime.Builders;
+using ODataRuntime.Impl.Models;
+using ODataRuntime.Interfaces;
+
+namespace ODataRuntime.Impl.ApiControllers
+{
+    public class PeopleApi: Api<Person>
+    {
+        public override void Register(ControllerBuilder controllerBuilder)
+        {
+            controllerBuilder.AddODataRoutePrefix(nameof(Person));
+            controllerBuilder.AddVersion("0.3");
+            var actionBuilderGet = new ActionBuilderFromBaseMethod(controllerBuilder, "Get", "DoGet");
+        }
+    }
+}
---
+++ ODataRuntime.Impl/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using ODataRuntime.Models;
+
+namespace ODataRuntime.Impl.Models
+{
+    public class Person : EntityKeyInt
+    {
+        public int Age { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
